44db9f9bbf20eecd51ca9db364a0f25d24a6445d
4m9fk <4m9fka@gmail.com>
2017-02-18 14:30:32 +0300

    Добавлена провекра количества фигур

--- chess2/chess.js
+++ chess2/chess.js
@@ -5,8 +5,6 @@ var ROW_CLASS = 'row';
 var CHESSBOARD_CLASS = 'chessboard';
 
 
-
-
 function Board(w, h) {
     this.w = w;
     this.h = h;
@@ -15,6 +13,7 @@ function Board(w, h) {
         var A = 'A'.charCodeAt(0);
         return String.fromCharCode(i + A - 1);
     }
+
     this.generateBoard = function (boardParent) {
         var p = document.querySelector(boardParent);
         var board = document.createElement('table');
@@ -54,31 +53,54 @@ function Board(w, h) {
 
     function getFigure(f, color) {
         switch (f) {
-        case 'pawn':
-            return (color == 'white') ? "<img src='https://upload.wikimedia.org/wikipedia/commons/0/04/Chess_plt60.png'>" :
-                "<img src='https://upload.wikimedia.org/wikipedia/commons/c/cd/Chess_pdt60.png'>";
-        case 'rook':
-            return (color == 'white') ? "<img src='https://upload.wikimedia.org/wikipedia/commons/5/5c/Chess_rlt60.png'>" :
-                "<img src='https://upload.wikimedia.org/wikipedia/commons/a/a0/Chess_rdt60.png'>";
-        case 'knight':
-            return (color == 'white') ? "<img src='https://upload.wikimedia.org/wikipedia/commons/2/28/Chess_nlt60.png'>" :
-                "<img src='https://upload.wikimedia.org/wikipedia/commons/f/f1/Chess_ndt60.png'>";
-        case 'bishop':
-            return (color == 'white') ? "<img src='https://upload.wikimedia.org/wikipedia/commons/9/9b/Chess_blt60.png'>" :
-                "<img src='https://upload.wikimedia.org/wikipedia/commons/8/81/Chess_bdt60.png'>";
-        case 'queen':
-            return (color == 'white') ? "<img src='https://upload.wikimedia.org/wikipedia/commons/4/49/Chess_qlt60.png'>" :
-                "<img src='https://upload.wikimedia.org/wikipedia/commons/a/af/Chess_qdt60.png'>";
-        case 'king':
-            return (color == 'white') ? "<img src='https://upload.wikimedia.org/wikipedia/commons/3/3b/Chess_klt60.png'>" :
-                "<img src='https://upload.wikimedia.org/wikipedia/commons/e/e3/Chess_kdt60.png'>";
+            case 'pawn':
+                return (color == 'white') ? "<img src='https://upload.wikimedia.org/wikipedia/commons/0/04/Chess_plt60.png'>" :
+                    "<img src='https://upload.wikimedia.org/wikipedia/commons/c/cd/Chess_pdt60.png'>";
+            case 'rook':
+                return (color == 'white') ? "<img src='https://upload.wikimedia.org/wikipedia/commons/5/5c/Chess_rlt60.png'>" :
+                    "<img src='https://upload.wikimedia.org/wikipedia/commons/a/a0/Chess_rdt60.png'>";
+            case 'knight':
+                return (color == 'white') ? "<img src='https://upload.wikimedia.org/wikipedia/commons/2/28/Chess_nlt60.png'>" :
+                    "<img src='https://upload.wikimedia.org/wikipedia/commons/f/f1/Chess_ndt60.png'>";
+            case 'bishop':
+                return (color == 'white') ? "<img src='https://upload.wikimedia.org/wikipedia/commons/9/9b/Chess_blt60.png'>" :
+                    "<img src='https://upload.wikimedia.org/wikipedia/commons/8/81/Chess_bdt60.png'>";
+            case 'queen':
+                return (color == 'white') ? "<img src='https://upload.wikimedia.org/wikipedia/commons/4/49/Chess_qlt60.png'>" :
+                    "<img src='https://upload.wikimedia.org/wikipedia/commons/a/af/Chess_qdt60.png'>";
+            case 'king':
+                return (color == 'white') ? "<img src='https://upload.wikimedia.org/wikipedia/commons/3/3b/Chess_klt60.png'>" :
+                    "<img src='https://upload.wikimedia.org/wikipedia/commons/e/e3/Chess_kdt60.png'>";
         }
 
     }
-   function checkFigures(fgrs){
-       // тут будет проверка фигур
-       return 1;
-   }
+
+    function checkFigures(fgrs) {
+
+        var restrictions = {// можно было бы вынести в константу
+            pawnblack: 8,
+            pawnwhite: 8,
+            rookwhite: 2,
+            rookblack: 2,
+            knightwhite: 2,
+            knightblack: 2,
+            bishopwhite: 2,
+            bishopblack: 2,
+            queenwhite: 1,
+            queenblack: 1,
+            kingwhite: 1,
+            kingblack: 1
+        };
+        for (f in fgrs) {
+            var fgr = fgrs[f].figure + fgrs[f].color;
+            if (--restrictions[fgr] == 0) {
+                console.error('слишком много ' + fgr + ' в json');
+                return 0;
+            }
+        }
+        //todo проверка нескольких фигур на одной клетке
+        return 1;
+    }
 
     this.putFigures = function (fileName) {
         // 1. Создаём новый объект XMLHttpRequest
@@ -94,14 +116,15 @@ function Board(w, h) {
         } else {
 
             var figures = JSON.parse(xhr.responseText);
-            if (checkFigures(figures)){
-            for (var i = 0; i < figures.length; i++) {
-                if (figures[i].deleted == 0) {
-                    var cell = document.getElementById(figures[i].postion);
-                    cell.innerHTML = getFigure(figures[i].figure, figures[i].color);
+            if (checkFigures(figures)) {
+                for (var i = 0; i < figures.length; i++) {
+                    if (figures[i].deleted == 0) {
+                        var cell = document.getElementById(figures[i].postion);
+                        cell.innerHTML = getFigure(figures[i].figure, figures[i].color);
+                    }
                 }
-            }}
-            console.log(figures);
+            }
+            //  console.log(figures);
 
 
         }
@@ -109,8 +132,6 @@ function Board(w, h) {
 }
 
 
-
-
-var test = new Board(14, 14);
+var test = new Board(48, 56);
 test.generateBoard('.container-fluid');
 test.putFigures('figures.json');
