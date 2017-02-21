6c3fd699b2a9d3868ff8113e7e19a434d1114e67
4m9fk <4m9fka@gmail.com>
2017-02-21 19:03:01 +0300

    Добавлено дз с регулярными выражениями

---
+++ hw3/hw3.js
@@ -0,0 +1,69 @@
+function makeDanger(elem, text) {
+    elem.parentElement.className = 'form-group has-danger';
+    msg = elem.parentElement.getElementsByClassName('form-control-feedback')[0];
+    msg.innerHTML = text;
+
+}
+
+function makeSucces(elem, text) {
+    elem.parentElement.className = 'form-group has-success';
+    msg = elem.parentElement.getElementsByClassName('form-control-feedback')[0];
+    msg.innerHTML = text;
+
+}
+
+
+
+
+function checkPhone(elem) {
+    
+    if (/^\+7\(\d{3}\)-\d{3}-\d{2}-\d{2}$/.test(elem.value)) {
+        makeSucces(elem, 'Все получилось');
+    } else {
+        makeDanger(elem, 'Что-то не так! Телефон должен иметь вид:+7(123)-456-78-90')
+    }
+
+}
+
+function checkEmail(elem) {
+    if (/\w+@\w+.\w+/.test(elem.value)) {
+        makeSucces(elem, 'Все получилось');
+    } else {
+        makeDanger(elem, 'Что-то не так! Email должен иметь вид email@example.com')
+    }
+}
+function checkP(elem) {
+    if (/^[A-Z]{2} \d{7}$/.test(elem.value)) {
+        makeSucces(elem, 'Все получилось');
+    } else {
+        makeDanger(elem, 'Что-то не так! Код должен иметь вид BM 1234567')
+    }
+}
+function checkColor(elem) {
+    if (elem.value.math().length>0) {
+        makeSucces(elem, 'Все получилось');
+    } else {
+        makeDanger(elem, 'Что-то не так! Код должен иметь вид #123456 или #ABC')
+    }
+}
+function checkCell(elem) {
+    if (/^[A-H]{1}[1-8]{1}$/.test(elem.value)) {
+        makeSucces(elem, 'Все получилось');
+    } else {
+        makeDanger(elem, 'Что-то не так! Ячейка должна иметь вид A2')
+    }
+}
+function searchTime(elem) {
+    if (elem.value.match(/\d{2}[:-]\d{2}/g)) {
+        makeSucces(elem, elem.value.match(/\d{2}[:-]\d{2}/g));
+    } else {
+        makeDanger(elem, 'Ничего не нашел')
+    }
+}
+function searchNumbers(elem) {
+    if (elem.value.match(/\d+(\.\d+)?/g)) {
+        makeSucces(elem, elem.value.match(/\d+(\.\d+)?/g));
+    } else {
+        makeDanger(elem, 'Ничего не нашел')
+    }
+}
