251214761014c5a6e9ef7ae919b8408a5f801055
4m9fk <4m9fka@gmail.com>
2017-02-27 23:33:49 +0300

    fixes

--- hw5/voter/js/voter.js
+++ hw5/voter/js/voter.js
@@ -2,22 +2,29 @@
  * Created by Sirozhka on 27.02.2017.
  */
 function Voter(options) {
+
     var elem = document.querySelector(options.elem);
     var votes = elem.querySelector('.vote').innerHTML;
     var u = elem.querySelector('.up');
-     u.addEventListener('click', up);
     var d = elem.querySelector('.down');
-     d.addEventListener('click', down);
+    u.addEventListener('click', up);
+    u.onmousedown = d.onmousedown = function () {
+        return false
+    };
+    d.addEventListener('click', down);
+
     function up() {
-        votes++; updateVotes();
+        votes++;
+        updateVotes();
     }
 
     function down() {
-        votes--; updateVotes();
+        votes--;
+        updateVotes();
     }
 
     function updateVotes() {
-        elem.querySelector('.vote').innerHTML  =  votes;
+        elem.querySelector('.vote').innerHTML = votes;
     }
     this.setVote = function (vote) {
         votes = vote;
